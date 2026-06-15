4e25650f2cd54053001fccc00d772221629e2f97
Bambeauzled <33978177+BeauMahler@users.noreply.github.com>
2026-06-15 23:04:00 +0200

    part 2
    added searchbar functionality and started detail page

--- actions.js
+++ actions.js
@@ -1,3 +1,22 @@
+function init(){
+  var searchValue = document.getElementById("searchValue");
+  searchValue.addEventListener('keyup', function(){
+    let filter = this.value.toLowerCase();
+    let eventList = document.getElementById("resultTable").getElementsByTagName("tr");
+    for(let i=0; i< eventList.length; i++){
+      let event = eventList[i];
+
+      if(event.textContent.toLowerCase().includes(filter)){ //matches search value (show)
+        event.classList.remove('hidden');
+      }
+      else {  //does not match search value (don't show)
+        event.classList.add('hidden');
+      }
+    }   
+    console.log(eventList);
+  });
+}
+
 async function getAPIData() {
   try {
     const response = await fetch("https://proxy.corsfix.com/?https://www.rvo.nl/api/v1/opendata/events");
@@ -6,7 +25,7 @@ async function getAPIData() {
     }
 
     const result = await response.json();
-    console.log(result[0]);
+    //console.log(result[0]);
 
     //fill table with resulting data
     var idCounter = 1;
@@ -39,10 +58,48 @@ async function getAPIData() {
       })
       
       //fill table rows with data
-      resultTable.innerHTML += "<tr><td>" + idCounter + "</td><td>" + item.title + "</td><td>" + landen + "</td><td>" + beginDates + "</td><td>" + endDates + "</td></tr>"
+      resultTable.innerHTML += "<tr><td>" + idCounter + "</td>" + 
+                               "<td><a href='eventDetails.html?eventnumber=" + item.id + "'>" + item.title + "</a></td>" +
+                               "<td>" + landen + "</td>" +
+                               "<td>" + beginDates + "</td>" +
+                               "<td>" + endDates + "</td></tr>"
 
       idCounter++; //up counter
     });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getEventData() {
+  try {
+    const response = await fetch("https://proxy.corsfix.com/?https://www.rvo.nl/api/v1/opendata/events");
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    var url = window.location.href;
+    var eventId = url.substring(url.lastIndexOf('=') + 1); //strip final part of url for id
+
+    var eventItem = result.find(item => { //find correct event based on id 
+      return item.id === eventId;
+    })
+  
+
+    //TODO
+    document.getElementById("eventName").innerHTML = eventItem.title;
+    document.getElementById("eventIntro").innerHTML = eventItem.intro;
+    document.getElementById("eventDates").innerHTML = eventItem.intro;
+    document.getElementById("eventCountries").innerHTML = eventItem.intro;
+    document.getElementById("eventSectors").innerHTML = eventItem.intro;
+    document.getElementById("eventTargets").innerHTML = eventItem.intro;
+    // document.getElementById("eventSectors").innerHTML = eventItem.intro;
+
+    
+    // console.log(eventItem);
+    
   } catch (error) {
     console.error(error.message);
   }
