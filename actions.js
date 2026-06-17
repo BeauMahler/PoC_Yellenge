739b422b3ea5b7dfce3244ebb73d7d39541313f5
Bambeauzled <33978177+BeauMahler@users.noreply.github.com>
2026-06-17 17:06:06 +0200

    finished assignment 3
    Added fields with data on detailpage, made optimization function 'concatNames'

--- actions.js
+++ actions.js
@@ -1,6 +1,11 @@
 function init(){
-  var searchValue = document.getElementById("searchValue");
-  searchValue.addEventListener('keyup', function(){
+  addEventListeners();
+  getAPIData();
+}
+
+function addEventListeners(){
+    var searchValue = document.getElementById("searchValue");
+    searchValue.addEventListener('keyup', function(){
     let filter = this.value.toLowerCase();
     let eventList = document.getElementById("resultTable").getElementsByTagName("tr");
     for(let i=0; i< eventList.length; i++){
@@ -13,7 +18,6 @@ function init(){
         event.classList.add('hidden');
       }
     }   
-    console.log(eventList);
   });
 }
 
@@ -25,24 +29,24 @@ async function getAPIData() {
     }
 
     const result = await response.json();
-    //console.log(result[0]);
 
     //fill table with resulting data
     var idCounter = 1;
     const resultTable = document.getElementById("resultTable");
     result.forEach(item => {
-      var landen = "";  //reset string
+      var countryNames = "";  //reset string
       var beginDates = "";
       var endDates = "";
       var numberOfCountries = item.countries.length - 1; //-1 because index starts at 0
       var numberOfDates = item.dates.length -1;
       
+      //-------FUTURE: rewrite to use concatNames function for this
       //if more than one country, add together in 1 string
       item.countries.forEach(country =>{
         if(item.countries.indexOf(country) == numberOfCountries){ //if last country in list
-          landen += country.name; //no final slash
+          countryNames += country.name; //no final slash
         }
-        else landen += country.name + " / ";
+        else countryNames += country.name + " / ";
       })
 
       //if more than one date, add together in 1 string
@@ -56,11 +60,12 @@ async function getAPIData() {
           endDates += date.end_value + "</br> & </br>";
         }
       })
+      //-------------
       
       //fill table rows with data
       resultTable.innerHTML += "<tr><td>" + idCounter + "</td>" + 
                                "<td><a href='eventDetails.html?eventnumber=" + item.id + "'>" + item.title + "</a></td>" +
-                               "<td>" + landen + "</td>" +
+                               "<td>" + countryNames + "</td>" +
                                "<td>" + beginDates + "</td>" +
                                "<td>" + endDates + "</td></tr>"
 
@@ -72,6 +77,7 @@ async function getAPIData() {
 }
 
 async function getEventData() {
+  //------FUTURE: create global variable with json response data instead of loose calls
   try {
     const response = await fetch("https://proxy.corsfix.com/?https://www.rvo.nl/api/v1/opendata/events");
     if (!response.ok) {
@@ -87,20 +93,47 @@ async function getEventData() {
       return item.id === eventId;
     })
   
-
-    //TODO
     document.getElementById("eventName").innerHTML = eventItem.title;
     document.getElementById("eventIntro").innerHTML = eventItem.intro;
-    document.getElementById("eventDates").innerHTML = eventItem.intro;
-    document.getElementById("eventCountries").innerHTML = eventItem.intro;
-    document.getElementById("eventSectors").innerHTML = eventItem.intro;
-    document.getElementById("eventTargets").innerHTML = eventItem.intro;
-    // document.getElementById("eventSectors").innerHTML = eventItem.intro;
+    document.getElementById("eventDates").innerHTML = concatNames(eventItem.dates, "dates");
+    document.getElementById("eventCountries").innerHTML = concatNames(eventItem.countries, "names");
+    document.getElementById("eventSectors").innerHTML = concatNames(eventItem.sectors, undefined);
+    document.getElementById("eventTargets").innerHTML = concatNames(eventItem.targets, undefined);
+    document.getElementById("eventTags").innerHTML = concatNames(eventItem.tags, undefined);
 
-    
-    // console.log(eventItem);
-    
+    var fullUrl = "https://www.rvo.nl" + eventItem.url;
+    document.getElementById("eventUrl").innerHTML = "<a href=" + fullUrl + ">" + fullUrl + "</a>";    
   } catch (error) {
     console.error(error.message);
   }
+}
+
+//function to concatenate multiple objects into one string, if applicable
+function concatNames(elementList, propertySelect){ 
+  var resultingString = ""; //reset string
+  var itemProperty;
+  var separationsymbol = ", ";  //default value
+
+  for(let i=0; i < elementList.length; i++){
+      let element = elementList[i];
+      itemProperty = element;   //default value
+
+      switch(propertySelect){ //apply correct property name and preferred separation symbol of element
+        case "names": 
+          itemProperty = element.name; 
+          separationsymbol = " / "
+          break;
+        case "dates": 
+          itemProperty = element.value + " tot " + element.end_value; 
+          separationsymbol = "<br/> & <br/>";
+          break;
+      }
+
+      if(i < elementList.length - 1){ //if not last item in list  
+        resultingString += itemProperty + separationsymbol; 
+      }
+      else resultingString += itemProperty;
+  }
+
+  return resultingString;
 }
